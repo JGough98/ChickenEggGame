74cb28e9f1a82a552498ea4a4b2727bd1cb91dd3
DESKTOP-A3B2H19\jwgou <jwgough98@gmail.com>
2023-11-02 18:41:41 +0000

    Added the player prefab.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+// Maybe start by trying to add controles and get it moving left right etc, in accordance to camera.
+// Then try adding a new speed when pressing x, which makes the chicken swich to the running animation.
+// Then we can try adding the egg respawn script and a respawn animation.
+// Start with a cube egg.
+// And do the respawn logic stuff.
+// Then we need to think of adding a jump.
+// Could try making simple animation too.
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
