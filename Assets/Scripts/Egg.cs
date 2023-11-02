852410a5891cc0caeb2ae634313008f8ab92fcfb
DESKTOP-A3B2H19\jwgou <jwgough98@gmail.com>
2023-11-02 18:41:55 +0000

    Other.

--- Assets/Scripts/Egg.cs
+++ Assets/Scripts/Egg.cs
@@ -33,7 +33,7 @@ public class Egg : MonoBehaviour, ITeleportPlayer
     // TODO - We will need to take into account that the eggs postion may out of bounds of the map.
     private Vector3 CurrentPostion => gameObject.transform.position;
 
-
+    // Woweee!
     public bool TeleportPlayer(Vector3 playerPostion)
     {
         throw new System.NotImplementedException();
