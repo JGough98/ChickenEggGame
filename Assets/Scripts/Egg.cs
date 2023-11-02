b7352fc59e8d4e0eccef79a0cd397f45c5c80ed2
DESKTOP-A3B2H19\jwgou <jwgough98@gmail.com>
2023-11-02 17:36:12 +0000

    Intial Commit.

---
+++ Assets/Scripts/Egg.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+
+public interface ISpawnAnimation
+{
+    public delegate void DisableControle();
+
+    public event DisableControle DisableMovement
+    {
+        add
+        { }
+        remove
+        { }
+    }
+
+
+    public void PlayAnimation();
+}
+
+
+public interface ITeleportPlayer
+{
+    bool TeleportPlayer(Vector3 playerPostion);
+}
+
+
+// The egg will be used to telport to the player to there previouse postion.
+// When the egg is on the ground a loner animation will play with a brief period where the player waits.
+public class Egg : MonoBehaviour, ITeleportPlayer
+{
+    private bool isAlive = false;
+
+    // TODO - We will need to take into account that the eggs postion may out of bounds of the map.
+    private Vector3 CurrentPostion => gameObject.transform.position;
+
+
+    public bool TeleportPlayer(Vector3 playerPostion)
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+    private void Start()
+    {
+
+    }
+}
