ff31a5d8b2ae45605d54bbde306455b2757faf8e
DESKTOP-A3B2H19\jwgou <jwgough98@gmail.com>
2024-10-13 15:51:15 +0100

    Created the black board data class to compose of all the objects the agents will need to know about.

---
+++ Assets/TowerDefence/Scripts/BlackBoard/BlackBoardData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BlackBoardData : MonoBehaviour
+{
+	[SerializeField]
+	public List<Recourse> Recourses;
+
+	[SerializeField]
+	public List<Drone> Drones;
+
+
+	public void Reset()
+	{
+		this.RefreshBlackBoard();
+	}
+}
