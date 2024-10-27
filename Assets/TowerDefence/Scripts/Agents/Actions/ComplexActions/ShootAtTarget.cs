d454032ee0eeb1e12b63e8f173e1bc904a6889b9
DESKTOP-A3B2H19\jwgou <jwgough98@gmail.com>
2024-10-27 14:39:11 +0000

    Started to implment the shoot at target complex action.

---
+++ Assets/TowerDefence/Scripts/Agents/Actions/ComplexActions/ShootAtTarget.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine.AI;
+
+
+namespace Assets.TowerDefence.Scripts.Agents.Actions.ComplexActions
+{
+	using BlackBoard;
+	using InstructionData;
+	using Interfaces;
+	using IntializeData;
+	using System;
+
+	public class ShootAtTarget : IInitializeAction<CollectAllRecoursesOfTypeInstructionsData, CollectAllRecoursesOfTypeIntializeData>
+	{
+		private BlackBoardSceneData blackBoardSceneData;
+
+		private NavMeshAgent agent;
+
+		private IStartAction<IPosition> rotateTowardsAction;
+
+		private IStartAction<ShootActionSetup> shootAction;
+
+		private IAction shootRotate;
+
+
+		public void Cancle()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Intialize(CollectAllRecoursesOfTypeIntializeData intializeData)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool IsFinished()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool Start(CollectAllRecoursesOfTypeInstructionsData instructions)
+		{
+			throw new NotImplementedException();
+
+			/*rotateTowardsAction.Start();
+			shootAction.Start();*/
+		}
+	}
+}
