550dbca80da7aa02377ad9a238184597819b28cf
DESKTOP-A3B2H19\jwgou <jwgough98@gmail.com>
2024-10-27 14:33:44 +0000

    Added new intialize action scripts.

---
+++ Assets/TowerDefence/Scripts/Agents/Actions/IntializeData/RotateActionSetup.cs
@@ -0,0 +1,18 @@
+﻿namespace Assets.TowerDefence.Scripts.Agents.Actions.IntializeData
+{
+	using Interfaces;
+
+
+	public struct RotateActionSetup
+	{
+		public IRotationSetter Agent
+		{
+			get;
+		}
+
+		public float RotationSpeed
+		{
+			get;
+		}
+	}
+}
---
+++ Assets/TowerDefence/Scripts/Agents/Actions/IntializeData/ShootActionSetup.cs
@@ -0,0 +1,38 @@
+﻿namespace Assets.TowerDefence.Scripts.Agents.Actions.IntializeData
+{
+	using BaseAction;
+	using Interfaces;
+
+
+	public struct ShootActionSetup
+	{
+		public float RateOfFire
+		{
+			get;
+			private set;
+		}
+
+		public Bullet Bullet
+		{
+			get;
+			private set;
+		}
+
+		public IFacingDirection FacingDirection
+		{
+			get;
+			private set;
+		}
+
+
+		public ShootActionSetup(
+			float rateOfFire,
+			Bullet bullet,
+			IFacingDirection facingDirection)
+		{
+			this.RateOfFire = rateOfFire;
+			this.Bullet = bullet;
+			this.FacingDirection = facingDirection;
+		}
+	}
+}
