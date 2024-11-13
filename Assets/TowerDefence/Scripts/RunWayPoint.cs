e76c5bba84e3bd074aa6abaa1a8756fd012706c0
DESKTOP-A3B2H19\jwgou <jwgough98@gmail.com>
2024-11-13 11:23:05 +0000

    Renamed classes.

--- Assets/TowerDefence/Scripts/RunWayPoint.cs
+++ Assets/TowerDefence/Scripts/RunWayPoint.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 
-public class EnamyWayPoint : MonoBehaviour
+public class RunWayPoint : MonoBehaviour
 {
 	[SerializeField]
 	private Transform ground;
---
+++ Assets/TowerDefence/Scripts/RunWaySpawner.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+namespace Assets.TowerDefence.Scripts
+{
+	using Agents;
+	using Agents.Actions.BaseAction;
+	using Agents.Actions.Interfaces;
+	using Utility;
+
+
+	public class RunWaySpawner : MonoBehaviour
+	{
+		[SerializeField]
+		private List<RunWayPoint> runWayPoints;
+
+
+		public IAction SpwanAtRunWay<T>(T enamy) where T : MonoBehaviour, IAgent
+		{
+			var newEnamy = GameObject.Instantiate<T>(enamy);
+
+			return CreateMoveThroughRunWayAction(newEnamy.NavMeshAgent);
+		}
+
+
+		private IAction CreateMoveThroughRunWayAction(NavMeshAgent agent)
+		{
+			var moveToAction = new ActionMoveToPoint();
+
+			moveToAction.Intialize(agent);
+
+			return ActionCombinerUtility.CombinedAction(
+				runWayPoints
+					.Select(x => ((IAction)moveToAction, (Action)(() => moveToAction.Start(x.RandomWayPointPostion))))
+					.ToArray());
+		}
+	}
+}
