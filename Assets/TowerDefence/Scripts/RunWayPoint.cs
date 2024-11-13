b21c09bd36d53cfd1300266b20d4d0eca0837c5d
DESKTOP-A3B2H19\jwgou <jwgough98@gmail.com>
2024-11-13 13:51:36 +0000

    Moved class into namespace.

--- Assets/TowerDefence/Scripts/RunWayPoint.cs
+++ Assets/TowerDefence/Scripts/RunWayPoint.cs
@@ -1,43 +1,48 @@
 using UnityEngine;
 
 
-public class RunWayPoint : MonoBehaviour
+namespace Assets.TowerDefence.Scripts
 {
-	[SerializeField]
-	private Transform ground;
-
-
-	private Vector2 HalfTopDownSize => new Vector2(
-		transform.localScale.x/2,
-		transform.localScale.z/2);
-
-	private Vector2 RandomTopDownsize
+	// TODO - Would be nice to set the Ground refrence on reset,
+	// could do this by searching for nav mesh agents checking their navmesh type.
+	public class RunWayPoint : MonoBehaviour
 	{
-		get
-		{
-			var halfTopDownSize = HalfTopDownSize;
+		[SerializeField]
+		private Transform ground;
 
-			return new Vector2(
-				Random.Range(
-					-halfTopDownSize.x,
-					halfTopDownSize.x),
-				Random.Range(
-					-halfTopDownSize.y,
-					halfTopDownSize.y));
+
+		private Vector2 HalfTopDownSize => new Vector2(
+			transform.localScale.x / 2,
+			transform.localScale.z / 2);
+
+		private Vector2 RandomTopDownsize
+		{
+			get
+			{
+				var halfTopDownSize = HalfTopDownSize;
+
+				return new Vector2(
+					Random.Range(
+						-halfTopDownSize.x,
+						halfTopDownSize.x),
+					Random.Range(
+						-halfTopDownSize.y,
+						halfTopDownSize.y));
+			}
 		}
-	}
 
 
-	public Vector3 RandomWayPointPostion
-	{
-		get
+		public Vector3 RandomWayPointPostion
 		{
-			var randomTopDownSize = RandomTopDownsize;
+			get
+			{
+				var randomTopDownSize = RandomTopDownsize;
 
-			return new Vector3(
-				transform.position.x + randomTopDownSize.x,
-				ground.position.y,
-				transform.position.z + randomTopDownSize.y);
+				return new Vector3(
+					transform.position.x + randomTopDownSize.x,
+					ground.position.y,
+					transform.position.z + randomTopDownSize.y);
+			}
 		}
 	}
 }
