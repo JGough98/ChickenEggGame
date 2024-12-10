1e22dafdabe380ae532218b0cac58ad86197f2e5
DESKTOP-A3B2H19\jwgou <jwgough98@gmail.com>
2024-12-10 15:26:36 +0000

    Added new ObjectPlacerType SO to add new shown and placed objects.

---
+++ Assets/TowerDefence/ScriptableObjects/ObjectPlacerItem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+namespace Assets.TowerDefense.ScriptableObjects
+{
+	[CreateAssetMenu(fileName = "ObjectPlacerItem-", menuName = "ScriptableObjects/ObjectPlacerItem", order = 6)]
+	public class ObjectPlacerItem : ScriptableObject
+	{
+		[SerializeField]
+		private GameObject shown;
+		[SerializeField]
+		public GameObject placed;
+
+
+		public GameObject Shown => shown;
+		public GameObject Placed => placed;
+	}
+}
