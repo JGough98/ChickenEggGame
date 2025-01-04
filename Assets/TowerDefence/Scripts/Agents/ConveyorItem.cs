405ce50f1f7c7e42e85a1a64e32f9f9b540fbf96
DESKTOP-A3B2H19\jwgou <jwgough98@gmail.com>
2025-01-04 16:43:58 +0000

    Created a simple scene to test the factory creation.

--- Assets/TowerDefence/Scripts/Agents/ConveyorItem.cs
+++ Assets/TowerDefence/Scripts/Agents/ConveyorItem.cs
@@ -3,6 +3,12 @@
 
 namespace Assets.TowerDefense.Scripts.Agents
 {
+	public enum ConveyorItemRecourseType
+	{
+		IRON,
+		FIRE
+	}
+
 	[RequireComponent(typeof(Rigidbody))]
 	public class ConveyorItem : MonoBehaviour, IDestroyedEvent<ConveyorItem>
 	{
@@ -12,8 +18,13 @@ namespace Assets.TowerDefense.Scripts.Agents
 		[SerializeField]
 		private Rigidbody rigidBody;
 
+		[SerializeField]
+		private ConveyorItemRecourseType conveyorItemRecourseType;
+
 
 		public Rigidbody RigidBody => rigidBody;
+
+		public ConveyorItemRecourseType ConveyorItemRecourseType => conveyorItemRecourseType;
 
 
 		/// <summary>
