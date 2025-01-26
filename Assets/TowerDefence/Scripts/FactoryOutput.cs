34cc3b4847c31802b3568c1e08da6a73c3033a66
DESKTOP-A3B2H19\jwgou <jwgough98@gmail.com>
2025-01-26 11:50:51 +0000

    Updating bullets.

--- Assets/TowerDefence/Scripts/FactoryOutput.cs
+++ Assets/TowerDefence/Scripts/FactoryOutput.cs
@@ -1,8 +1,26 @@
-﻿using UnityEngine;
+﻿using Assets.TowerDefense.Scripts.Agents;
+using System.Collections.Generic;
+using UnityEngine;
 
 
 namespace Assets.TowerDefense.Scripts
 {
+	// So this should take in a set of recourses and check if this can be combined together.
+	public class FactoryRecourseBuilder
+	{
+		public bool CanCreate(
+			List<ConveyorItemRecourseType> conveyorItemRecourseTypes,
+			out GameObject createdObject)
+		{
+			var canCreate = false;
+			createdObject = null;
+
+
+			return canCreate;
+		}
+	}
+
+
 	public class FactoryOutput : MonoBehaviour
 	{
 		public event AnimationFinished OnOutputProcessed;
